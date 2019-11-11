ae7e605654834d003c5c5aa24ca0c635ac0e801a
Jing Lin <jingglin@mit.edu>
2019-11-11 14:23:04 -0500

    fixed cda

--- cda_draft.py
+++ cda_draft.py
@@ -24,31 +24,25 @@ def split_on_var(assignment, non_assigned_var, csp):
         local_assignment = assignment.copy()
         local_assignment[non_assigned_var] = value
         queue.append(local_assignment)
-    #print ("split", queue)
     return queue
 
-#def sort_priority(queue, optimal_csp):
-#    # sort queue from largest to smallest log probability
-#    priority_queue = []
-#    for assignment in queue:
-#        score = optimal_csp.reward_f(assignment)
-#        priority_queue.append((score, assignment))
-#    
-#    priority_queue = sorted(priority_queue, key=lambda x: x[0], reverse=True)
-#    #print ("priority queue", priority_queue)
-#
-#    new_queue = []
-#    for s, assign in priority_queue:
-#        new_queue.append(assign)
-#    return new_queue
+def sort_priority(queue, optimal_csp):
+    # sort queue from largest to smallest log probability
+    priority_queue = []
+    for assignment in queue:
+        score = optimal_csp.reward_f(assignment)
+        priority_queue.append((score, assignment))
+    
+    priority_queue = sorted(priority_queue, key=lambda x: x[0], reverse=True)
+    #print ("priority queue", priority_queue)
+
+    new_queue = []
+    for s, assign in priority_queue:
+        new_queue.append(assign)
+    return new_queue
 
 
-
-
-
-
-
-def cda(optimal_csp,output):
+def cda(optimal_csp,output, states):
     queue = [{}]
     conflicts = []
     expanded = []
@@ -57,57 +51,65 @@ def cda(optimal_csp,output):
         expanded.append(assign)
         is_full_assign, non_assigned_var = check_full_assignment(assign)
         if is_full_assign:
-            is_consistent, conflict = optimal_csp.backtrack_search_conflict(assign, {})
+            csp_assignment = optimal_csp.backtrack_search(assign)
             
-            print('\n\tConsistent: {} \n\tIT FOUND A FULL ASSIGNMNET {} with conflict {}'.format(is_consistent, assign,conflict))
-            if is_consistent:
+            if csp_assignment is not None:
                 print('\tIt found a full assignment that was consistent')
                 return assign
             else:
-                conflicts.append(conflict)
-                queue,conflicts = remove_conflict(conflict, queue,output,conflicts)
-#                print('Output queue at this point {}'.format(queue))
+                queue,conflicts = remove_conflict(assign, queue,output,conflicts, states)
+##                print('Inconsistent, Output queue at this point {} with conflicts {}'.format(queue, conflicts))
         else:
             is_resolved, unresolved_conflict = resolved_conflicts(conflicts, assign, optimal_csp)
             if is_resolved:
-#                print('The intial pass that it goes through as it builds up the assignment {}'.format(assign))
                 nbrs = split_on_var(assign, non_assigned_var, optimal_csp)
-#                print('The neighbors are {}'.format(nbrs))
+                #print('Conflict resolved with assign {}, The neighbors are {}'.format(assign, nbrs))
             else:
                 nbrs = split_on_conflict(assign, unresolved_conflict, optimal_csp)
-                print('The neighbors it found through split on conflict are {}'.format(nbrs))
+##                print ("Conflict not resolved: {}, with assign: {}".format(unresolved_conflict, assign))
+##                print('The neighbors it found through split on conflict are {}'.format(nbrs))
             for nbr in nbrs:
                 if nbr not in expanded:
                     queue.append(nbr)
-#            for each in queue:  
-#                print('THe current queue status  {}'.format(each))
-#        print('-----------------------------------------------------')
 
-#        queue = sort_priority(queue, optimal_csp)
+        queue = sort_priority(queue, optimal_csp)
     return None
 
 
 
 
 
-def remove_conflict(conflict, queue,output,conflict_list):
+def remove_conflict(conflict, queue,output,conflict_list, states):
     ## output needs to be true or False 
     # remove any states in Q that manifest conflict
     ### find conflict order first 
     issue=[]
     temp_issue=[]
     final_issue={}
-#    print('\n----------------------------------------------------------')
-#    print('remind me again what is in conflict {}'.format(conflict))
-#    print('remind me again of what the prior conflcits were {}'.format(conflict_list))
     
-    
+    # need to update this, right now hard coded
     while len(issue)==0:
         for var in range(4,0,-1):
             temp_issue=[]
             str_var=str(var)
             num_var=var
-            if conflict[str_var] !=output:
+            is_normal = conflict[str_var]
+            is_open = states[str_var]
+            # valve is open, and nominal: has flow
+            if is_normal and is_open:
+                expected = 1
+            elif is_normal and not is_open:
+                # no flow
+                expected = 0
+            elif not is_normal and is_open:
+                # stuck close
+                expected = 0
+            else:
+                # broken, and state is close
+                # stuck open
+                expected = 1
+            
+            if expected !=output:
                 while num_var>0:
                     temp_issue.append(num_var)
                     num_var=num_var-2
@@ -115,81 +117,57 @@ def remove_conflict(conflict, queue,output,conflict_list):
                     issue=temp_issue
                     break
     
-#    print('the conflict it found for this case was {}'.format(issue))
-#    print('the total list of conflcits at this point is {}'.format(conflict_list))
     for each in issue:
         each_string=str(each)
         final_issue[each_string]=conflict[each_string]
         
-    print('FINALIZED the conflict it found for this case was {}'.format(final_issue))
-#    print('the total list of conflcits at this point is {}'.format(conflict_list))    
+##    print('FINALIZED the conflict it found for this case was {}'.format(final_issue))   
         
     conflict_list.append(final_issue)
     
-    
 
-    
-    
     new_queue = []
     for assignment in queue:
-#        print('Initial assignment {}'.format(assignment))
-        is_conflicted =0
-        for var in assignment:
-#            print('Current Var is {}'.format(var))
-            if assignment[var] !=output and var in final_issue:
-#                print('went here {} {}'.format(assignment[var],output))
-                is_conflicted=is_conflicted+1
-            elif is_conflicted>0 and var in final_issue:
-                is_conflicted=is_conflicted+1
-#        print('For assignment {} \n\tthe resultant estimation was {}'.format(assignment,is_conflicted))    
-        if is_conflicted == len(final_issue):
-#            print('The Assingments that get assigned to queue {}'.format(assignment))
-            # check all conflict variable, does not conflict with assignment
+        if not final_issue.items() <= assignment.items():
             new_queue.append(assignment)
     return new_queue,conflict_list
 
             
+
 def build_conflict_kernel(conflict, csp):
-    kernels = {}
-    # conflict: not ("1"= True and "2"=True)
-    # resolve to ("1"=False) or ("2"=False)
+    kernel = {}
+    # conflict: not ("1"=True and "2"=True)
+    # resolve t ("1" = False) or ("2"=False)
     for var in conflict.keys():
         value = conflict[var]
-        kernels[var] = []
-        for other_value in csp.domains[var]:
-            if other_value != value:
-                # same as constraint
-                # reduced down domain range
-                # OR relationship, either one of value is True
-                kernels[var].append({var: other_value})
-    return kernels
+        # since we only have either True or False
+        kernel[var] = not value
+    return kernel
+    
 
 def resolved_conflicts(conflicts, assignment, csp):
-    all_constraints = []
+    kernels = []
     for conflict in conflicts:
+        # build a kernel for each conflict
         kernel = build_conflict_kernel(conflict, csp)
-        all_constraints.append(kernel)
-    
-    # resolve in the sense of consistent with all_constraints
-    # since we rewrite kernel into constraint
-
-    # all satisfied: And relationship
-    all_satisfied = True
-    for c in all_constraints:
-        # c satisfied: Or relationship
-        c_satisfied = False
-        for var in c.keys():
-            desired_value = c[var]
+        kernels.append(kernel)
+        
+    for i in range(len(kernels)):
+        k = kernels[i]
+        is_resolved = False
+        # check if any one of variable is resolved for each kernel
+        # resolved means: kernel value of var is same as our assignment of var
+        for var in k:
             if var in assignment:
-                actual_value = assignment[var]
-                if desired_value == actual_value:
-                    c_satisfied = True
+                # check resolvable
+                if k[var] == assignment[var]:
+                    is_resolved = True
                     break
-        if not c_satisfied:
-            return False, c
             
+        if not is_resolved:
+            # ith kernel not resolvable, so conflict i is returned
+            return False, conflicts[i]
     return True, None
-
 
 def split_on_conflict(assignment, unresolved_conflict, csp):
     kernel = build_conflict_kernel(unresolved_conflict, csp)
@@ -245,10 +223,38 @@ types = {"1": 0, "2": 0, "3": 1, "4": 1}
 valve_prob = [0.8, 0.1, 0.1]
 pyro_prob = [0.95, 0.03, 0.02]
 probs_table = [valve_prob, pyro_prob]
-score_f=0; ## JUST TO INITIALIZE
+# reward function f = g(x) + h(x)
+def score_f(assignment):
+    score = 0
+    for var in decision_vars:
+        valve_type = types[var]
+        prob_dist = probs_table[valve_type]
+        state = states[var]
+        if var in assignment:
+            is_normal = assignment[var]
+            if is_normal:
+                #print ("var: ", var, "is normal", "prob", prob_dist[0])
+                score += np.log(prob_dist[0])
+            else:
+                if state == 1:
+                    #print ("var: ", var, "not normal, prob", prob_dist[1])
+                    # suppose to be open, but detect not normal, thus stuck close
+                    score += np.log(prob_dist[1])
+                else:
+                    #print ("var: ", var, "not normal, prob", prob_dist[2])
+                    # suppose to be close, but detect not normal, thus stuck open
+                    score += np.log(prob_dist[2])
+
+        else:
+            #print("var: ", var, "estimate: ", np.max(prob_dist))
+            # estimate heuristic
+            # overestimate 
+            score += np.max(np.log(prob_dist))
+    #print ("score: ", score)
+    return score
 
 
 optimal_csp = OptimalCSP(variables, domains, constraints, decision_vars, score_f)
 output=False
-result_cda = cda(optimal_csp,output)
+result_cda = cda(optimal_csp,output, states)
 print (result_cda)
